713c1614f1241858d2a96222b474fb9fb85c05a0
luis.montano <luis.montano@javeriana.edu.co>
2020-07-06 11:26:14 -0500

    beam solver

---
+++ src/test/java/com/structuralanalisis/structuralanalisis/StructuralanalisisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.structuralanalisis.structuralanalisis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuralanalisisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
